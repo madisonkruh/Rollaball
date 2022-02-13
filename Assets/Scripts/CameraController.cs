426db487e60e307c95dfb22de6dbf6afb28b517b
Madison Kruh <madisonkruh@gmail.com>
2022-02-13 10:32:27 -0500

    Roll-a-ball game on Unity, moveable Player and Camera, set up play area, added collectibles

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // references Player's position
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // current transform position of Camera - transform position of Player  
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame, but runs after all other updates are done
+    // so Camera position is only set once player has moved for that frame
+    void LateUpdate()
+    {
+        // adjusts Camera position whenever Player moves
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // spped starting value 0
+    public float speed = 0;
+
+    // will hold reference to Rigidbody being accessed
+    private Rigidbody rb;
+
+    // how many Pickup objects Player has collected
+    private int count;
+
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // reference to Rigidbody component attached to the Player sphere
+        rb = GetComponent<Rigidbody>();
+
+        // set intial value to 0
+        count = 0;
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        // gets Vector2 data from movementValue and stores it in Vector2 variable movementVector
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // add force to Rigidbody of Player sphere
+        rb.AddForce(movement * speed);
+    }
+
+    // detects contact between Player object and Pickup object without creating collision
+    // called by Unity when Player object first touches a trigger collider
+    // will be passed a reference to trigger collider that has been touched (other)
+    private void OnTriggerEnter(Collider other)
+    {
+        // if other is tagged PickUp (ie. is a PickUp Prefab)
+        if(other.gameObject.CompareTag("PickUp")){
+            // deactivate 
+            other.gameObject.SetActive(false);
+        }
+    }
+}
